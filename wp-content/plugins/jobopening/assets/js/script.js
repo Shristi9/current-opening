5b3f44f11eac3b7efd8f164b559cdfbefce049b2
shristi9 <shristijha0202@gmail.com>
2021-05-24 09:06:37 +0530

    Added

---
+++ wp-content/plugins/jobopening/assets/js/script.js
@@ -0,0 +1,22 @@
+	$(document).ready(function(){
+
+
+	$(".cstm_drop").chosen();
+	
+	
+   $(".cstm_technology").chosen();
+
+  $(".cstm_experience").chosen();
+   $("._cstm_notice").chosen();
+   $("._cstm_branch").chosen();
+  $(".chosen-search-input").addClass("cstm_search");
+$(".chosen-search-input").attr("placeholder", "Type to search...");
+
+
+
+$("a.chosen-single span").addClass("cstm_shri");	
+
+
+});
+
+	
